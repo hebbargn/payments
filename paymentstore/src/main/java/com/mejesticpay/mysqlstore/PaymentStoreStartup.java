825cc849c93819824af29ae53f7ed2a484940daa
Naga Hebbar <hebbargn@gmail.com>
2019-09-28 22:54:59 -0400

    Updated to support command store

--- paymentstore/src/main/java/com/mejesticpay/mysqlstore/PaymentStoreStartup.java
+++ paymentstore/src/main/java/com/mejesticpay/mysqlstore/PaymentStoreStartup.java
@@ -1,19 +1,15 @@
 package com.mejesticpay.mysqlstore;
 
-import com.mejesticpay.mysqlstore.jpa.BaseJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(repositoryBaseClass = BaseJpaRepositoryImpl.class)
-
 public class PaymentStoreStartup
 {
 
     public static void main(String []args)
-        {
-            SpringApplication.run(PaymentStoreStartup.class,args);
-        }
+    {
+        SpringApplication.run(PaymentStoreStartup.class,args);
+    }
 
 }
